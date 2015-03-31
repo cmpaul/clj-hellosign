70857b47aa6ace422a58dc53c894ccaf3e680d7c
Chris Paul <chris@hellosign.com>
2015-03-30 20:52:46 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-hellosign "0.1.0-SNAPSHOT"
+  :description "A clojure wrapper for HelloSign"
+  :url "http://github.com/HelloFax/clj-hellosign"
+  :license {:name "MIT license"
+            :url "https://github.com/HelloFax/clj-hellosign/blob/v3/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+    [org.clojure/data.json "0.2.6"]
+    [clj-http "1.1.0"]])
---
+++ test/clj_hellosign/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-hellosign.core-test
+  (:require [clojure.test :refer :all]
+            [clj-hellosign.core :refer :all]))
+
+(deftest a-test
+  (testing "Test test :)"
+    (is (= 0 0))))
